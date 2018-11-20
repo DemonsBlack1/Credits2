8d53b2e33ac18e96cb1de5a89849b59885345a04
DemonsBlack1 <44532514+DemonsBlack1@users.noreply.github.com>
2018-11-20 19:25:19 +0200

    Create Credits2.js

---
+++ Credits2.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("ٍPotato#Army");
+
+client.on("ready", () => {
+let channel =     client.channels.get("509339743662768138")
+setInterval(function() {
+channel.send(`DB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestBestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is BestDB is the BestDB is the BestDB is the BestDB is the BestDB is the BestDB is`);
+
+}, 25)
+})
+ 
+client.login('NTA4OTgzNTg3OTcxMDA2NDc0.DtXQaA.N3dbAW6H6qjhAWbV5WMr2ieDP5c');
